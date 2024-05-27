5a20e3fa983d024454b4c56149bb791ca2ec2212
betto <betto_beto@hotmail.com>
2024-05-27 13:29:12 -0300

    Create cypress project

---
+++ cypress/support/commands.js
@@ -0,0 +1,9 @@
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
+
+    cy.get('#firstName').type('Walmyr')
+    cy.get('#lastName').type('Filha')
+    cy.get('#email').type('walmyr@exemplo.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+
+})
